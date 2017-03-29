a1a42e862b4653aa74fa253774b6eb7ea46d2b75
Aaron Dotson <addotson@uaa.alaska.edu>
2017-03-28 16:24:40 -0800

    Update lvl-logger.ino

--- lvl-logger.ino
+++ lvl-logger.ino
@@ -1,5 +1,30 @@
 #include "LowPower.h"
-#include<SPIFlash.h>
+#include <RFM69.h>          //get it here: https://www.github.com/lowpowerlab/rfm69
+#include <RFM69_ATC.h>      //get it here: https://www.github.com/lowpowerlab/rfm69
+#include<SPIFlash.h>        //get it here: https://www.github.com/lowpowerlab/spiflash
+#include <SPI.h>            //included with Arduino IDE install (www.arduino.cc)
+
+//*********************************************************************************************
+//************ IMPORTANT SETTINGS - YOU MUST CHANGE/CONFIGURE TO FIT YOUR HARDWARE ************
+//*********************************************************************************************
+#define NODEID        2    //must be unique for each node on same network (range up to 254, 255 is used for broadcast)
+#define NETWORKID     100  //the same on all nodes that talk to each other (range up to 255)
+#define GATEWAYID     1
+//Match frequency to the hardware version of the radio on your Moteino (uncomment one):
+#define FREQUENCY   RF69_433MHZ
+//#define FREQUENCY   RF69_868MHZ
+//#define FREQUENCY     RF69_915MHZ
+#define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
+//#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
+//*********************************************************************************************
+//Auto Transmission Control - dials down transmit power to save battery
+//Usually you do not need to always transmit at max output power
+//By reducing TX power even a little you save a significant amount of battery power
+//This setting enables this gateway to work with remote nodes that have ATC enabled to
+//dial their power down to only the required level (ATC_RSSI)
+#define ENABLE_ATC    //comment out this line to disable AUTO TRANSMISSION CONTROL
+#define ATC_RSSI      -80
+//*********************************************************************************************
 
 #define SERIAL_BAUD      115200
 #define LED           9 // Moteinos have LEDs on D9
@@ -8,7 +33,20 @@
 uint32_t prevOffset=0;
 uint32_t Offset=0;
 
-SPIFlash flash(FLASH_SS, 0xEF30);
+
+int TRANSMITPERIOD = 200; //transmit a packet to gateway so often (in ms)
+byte payload;
+char buff[20];
+byte sendSize=0;
+boolean requestACK = false;
+SPIFlash flash(FLASH_SS, 0xEF30);  //EF30 for 4mbit  Windbond chip (W25X40CL)
+
+#ifdef ENABLE_ATC
+  RFM69_ATC radio;
+#else
+  RFM69 radio;
+#endif
+
 
 int USpower = 7;
 int USoutput = 5;
@@ -24,29 +62,152 @@ uint8_t average = 0;                // the average
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
-  Serial.print("Start...");
+  radio.initialize(FREQUENCY,NODEID,NETWORKID);
+#ifdef IS_RFM69HW
+  radio.setHighPower(); //uncomment only for RFM69HW!
+#endif
+  radio.encrypt(ENCRYPTKEY);
+  //radio.setFrequency(919000000); //set frequency to some custom frequency
+
+//Auto Transmission Control - dials down transmit power to save battery (-100 is the noise floor, -90 is still pretty good)
+//For indoor nodes that are pretty static and at pretty stable temperatures (like a MotionMote) -90dBm is quite safe
+//For more variable nodes that can expect to move or experience larger temp drifts a lower margin like -70 to -80 would probably be better
+//Always test your ATC mote in the edge cases in your own environment to ensure ATC will perform as you expect
+#ifdef ENABLE_ATC
+  radio.enableAutoPower(ATC_RSSI);
+#endif
+
+  char buff[50];
+  sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  Serial.println(buff);
 
   if (flash.initialize())
   {
-    Serial.println("Init OK!");
-    Blink(LED, 20, 10);
+    Serial.print("SPI Flash Init OK ... UniqueID (MAC): ");
+    flash.readUniqueId();
+    for (byte i=0;i<8;i++)
+    {
+      Serial.print(flash.UNIQUEID[i], HEX);
+      Serial.print(' ');
+    }
+    Serial.println();
   }
   else
-    Serial.println("Init FAIL!");
-  
-  delay(1000);
+    Serial.println("SPI Flash MEM not found (is chip soldered?)...");
+
+#ifdef ENABLE_ATC
+  Serial.println("RFM69_ATC Enabled (Auto Transmission Control)\n");
+#endif
 
   pinMode(USpower,OUTPUT);
   flash.chipErase();
 
 }
 
+long lastPeriod = 0;
 void loop() {
+   //process any serial input
+  if (Serial.available() > 0)
+  {
+    char input = Serial.read();
+    if (input >= 48 && input <= 57) //[0,9]
+    {
+      TRANSMITPERIOD = 100 * (input-48);
+      if (TRANSMITPERIOD == 0) TRANSMITPERIOD = 1000;
+      Serial.print("\nChanging delay to ");
+      Serial.print(TRANSMITPERIOD);
+      Serial.println("ms\n");
+    }
+
+    if (input == 'r') //d=dump register values
+      radio.readAllRegs();
+    //if (input == 'E') //E=enable encryption
+    //  radio.encrypt(KEY);
+    //if (input == 'e') //e=disable encryption
+    //  radio.encrypt(null);
+
+    if (input == 'd') //d=dump flash area
+    {
+      Serial.println("Flash content:");
+      uint16_t counter = 0;
+
+      Serial.print("0-256: ");
+      while(counter<=256){
+        Serial.print(flash.readByte(counter++), HEX);
+        Serial.print('.');
+      }
+      while(flash.busy());
+      Serial.println();
+    }
+    if (input == 'e')
+    {
+      Serial.print("Erasing Flash chip ... ");
+      flash.chipErase();
+      while(flash.busy());
+      Serial.println("DONE");
+    }
+    if (input == 'i')
+    {
+      Serial.print("DeviceID: ");
+      word jedecid = flash.readDeviceId();
+      Serial.println(jedecid, HEX);
+    }
+  }
+
+  //check for any received packets
+  if (radio.receiveDone())
+  {
+    Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
+    for (byte i = 0; i < radio.DATALEN; i++)
+      Serial.print((char)radio.DATA[i]);
+    Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
+
+    if (radio.ACKRequested())
+    {
+      radio.sendACK();
+      Serial.print(" - ACK sent");
+    }
+    Blink(LED,3);
+    Serial.println();
+  }
+
+  int currPeriod = millis()/TRANSMITPERIOD;
+  if (currPeriod != lastPeriod)
+  {
+    lastPeriod=currPeriod;
+
+    //send FLASH id
+    if(sendSize==0)
+    {
+      sprintf(buff, "FLASH_MEM_ID:0x%X", flash.readDeviceId());
+      byte buffLen=strlen(buff);
+      if (radio.sendWithRetry(GATEWAYID, buff, buffLen))
+        Serial.print(" ok!");
+      else Serial.print(" nothing...");
+      //sendSize = (sendSize + 1) % 31;
+    }
+    else
+    {
+      Serial.print("Sending[");
+      Serial.print(sendSize);
+      Serial.print("]: ");
+      Serial.print(payload);
+
+      if (radio.sendWithRetry(GATEWAYID, payload, sendSize))
+       Serial.print(" ok!");
+      else Serial.print(" nothing...");
+    }
+    //sendSize = (sendSize + 1) % 31;
+    Serial.println();
+    Blink(LED,3);
+  }
+   
    Serial.flush();
    // Enter power down state for 8 s with ADC and BOD module disabled
    // LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
    USreading();
    StoreData();
+   payload = average;
    
 }
 
@@ -102,13 +263,10 @@ void StoreData() {
 
 }
 
-void Blink(byte PIN, int DELAY_MS, byte loops) {
+void Blink(byte PIN, int DELAY_MS)
+{
   pinMode(PIN, OUTPUT);
-  while (loops--)
-  {
-    digitalWrite(PIN,HIGH);
-    delay(DELAY_MS);
-    digitalWrite(PIN,LOW);
-    delay(DELAY_MS);  
-  }
+  digitalWrite(PIN,HIGH);
+  delay(DELAY_MS);
+  digitalWrite(PIN,LOW);
 }
