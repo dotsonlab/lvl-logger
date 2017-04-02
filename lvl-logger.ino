8e2ee2c712cb7bccbebde74f022bef97e1654d45
Aaron Dotson <addotson@uaa.alaska.edu>
2017-04-01 22:01:33 -0800

    Update lvl-logger.ino

--- lvl-logger.ino
+++ lvl-logger.ino
@@ -1,128 +1,62 @@
-#include "LowPower.h"
-#include <RFM69.h>
-#include <SPI.h>
 #include<SPIFlash.h>
+#include "LowPower.h"
 
-#define NODEID      99
-#define NETWORKID   100
-#define GATEWAYID   1
-#define FREQUENCY   RF69_433MHZ //Match this with the version of your Moteino! (others: RF69_433MHZ, RF69_868MHZ)
-#define KEY         "sampleEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
-#define LED           9 // Moteinos have LEDs on D9
 #define SERIAL_BAUD      115200
-#define ACK_TIME    30  // # of ms to wait for an ack
-
+#define LED           9 // Moteinos have LEDs on D9
 #define FLASH_SS      8 // and FLASH SS on D8
-
-//int TRANSMITPERIOD = 1000; //transmit a packet to gateway so often (in ms)
-
-byte sendSize=0;
-boolean requestACK = false;
-SPIFlash flash(FLASH_SS, 0xEF30);
-RFM69 radio;
-
-uint32_t prevOffset=0;
-uint32_t Offset=0;
-
 
 int USpower = 7;
 int USoutput = 5;
 
-const int numReadings = 25;
+SPIFlash flash(FLASH_SS, 0xEF30);
+//Moteino R4 equipped with 2048 pages of 256 byte storage locations (0 - 255). Offsets from 0 - 524288 available
 
-int i;
+
+
+
+const int numReadings = 25;
 int readings[numReadings];      // the readings from the analog input
 int readIndex = 0;              // the index of the current reading
 int total = 0;                  // the running total
 int prevTotal = 0;              // prior total
 uint8_t average = 0;                // the average
 
-typedef struct {
-  char           cmd; //store this nodeId
-  //unsigned long uptime; //uptime in ms
-  byte         volume;   //volume
-} Payload;
-Payload theData;
+uint8_t DataInterval=15;        //0 - 255 minutes (whole numbers only)
+uint32_t DataOffset=0;          //need to initialize this in setup by looking in flash memory
+uint32_t PrevDataOffset=0;      //need to initialize this in setup by looking in flash memory
 
 String incomingCMD;
 
 void setup() {
   Serial.begin(SERIAL_BAUD);
   Serial.flush();
-  Serial.print("Starting...");
-  
-  radio.initialize(FREQUENCY,NODEID,NETWORKID);
-  //radio.setHighPower(); //uncomment only for RFM69HW!
-  radio.encrypt(KEY);
-  char buff[50];
-  sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
-  Serial.println(buff);
-  
+  Serial.print("Start...");
+
   if (flash.initialize())
-    Serial.println("SPI Flash Init OK!");
+  {
+    Serial.println("Init OK!");
+  }
   else
-    Serial.println("SPI Flash Init FAIL! (is chip present?)");
+    Serial.println("Init FAIL!");
 
   pinMode(USpower,OUTPUT);
-  flash.chipErase();
 
 }
-
-long lastPeriod = -1;
 
 void loop() {
-   
-  //check for any received packets
-  if (radio.receiveDone())
-  {
-    Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
-    for (byte i = 0; i < radio.DATALEN; i++)
-      Serial.print((char)radio.DATA[i]);
-    Serial.print("   [RX_RSSI:");Serial.print(radio.readRSSI());Serial.print("]");
+  // LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
 
-    if (radio.ACKRequested())
-    {
-      radio.sendACK();
-      Serial.print(" - ACK sent");
-      delay(10);
-    }
-    Blink(LED,5);
-    Serial.println();
-  }
-  
-//  int currPeriod = millis()/TRANSMITPERIOD;
-//  if (currPeriod != lastPeriod)
-//  {
-//    //fill in the struct with new values
-//    theData.cmd = NODEID;
-//    //theData.uptime = millis();
-//    theData.volume = average;
-//    
-//    Serial.print("Sending struct (");
-//    Serial.print(sizeof(theData));
-//    Serial.print(" bytes) ... ");
-//    if (radio.sendWithRetry(GATEWAYID, (const void*)(&theData), sizeof(theData)))
-//      Serial.print(" ok!");
-//    else Serial.print(" nothing...");
-//    Serial.println();
-//    Blink(LED,3);
-//    lastPeriod=currPeriod;
-//  }
-   
-   USreading();
-   //StoreData();
-   // Enter power down state for 8 s with ADC and BOD module disabled
-   // LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
 }
 
-void Blink(byte PIN, int DELAY_MS)
-{
-  pinMode(PIN, OUTPUT);
-  digitalWrite(PIN,HIGH);
-  delay(DELAY_MS);
-  digitalWrite(PIN,LOW);
+void getOffset() {
+    byte unwritten = 255; 
+    long i = 0;
+    while(flash.readByte(i) != unwritten) {
+      i++;
+      }
+    DataOffset = i;
+    i = 0;
 }
-
 
 void USreading() {
   //turn ultrasonic power ON
@@ -138,7 +72,7 @@ void USreading() {
   // if we're at the end of the array...
   if (readIndex >= numReadings) {
     // calculate the average:
-    for (i = 0; i < numReadings; i++) {
+    for (int i = 0; i < numReadings; i++) {
       total = prevTotal + readings[i];
       prevTotal = total;
     }
@@ -157,94 +91,63 @@ void USreading() {
 
 void StoreData() {
 
-    prevOffset = Offset;
-    Offset++;
+    PrevDataOffset = DataOffset;
+    DataOffset++;
+
+    //need to update table of contents here by writing updated values to flash memory
     
-    flash.writeByte(Offset, average);
+    flash.writeByte(DataOffset, average);
     
     Serial.print("Writing ");
     Serial.print(average);
     Serial.print(" to offset ");
-    Serial.print(Offset);
+    Serial.print(DataOffset);
     Serial.println(".");
 
     Serial.print("Last stored data was ");
-    Serial.print(flash.readByte(Offset));
+    Serial.print(flash.readByte(PrevDataOffset));
     Serial.print(" stored at offset ");
-    Serial.print(Offset);    
+    Serial.print(PrevDataOffset);    
     Serial.println(".");
 
-}
-
-void Blink(byte PIN, int DELAY_MS, byte loops) {
-  pinMode(PIN, OUTPUT);
-  while (loops--)
-  {
-    digitalWrite(PIN,HIGH);
-    delay(DELAY_MS);
-    digitalWrite(PIN,LOW);
-    delay(DELAY_MS);  
-  }
 }
 
 void serialEvent() {   //This interrupt will trigger when the data coming from the serial monitor(pc/mac/other) is received
   if (Serial.available() > 0) {
     incomingCMD = Serial.readStringUntil('\n');
 
-    if (incomingCMD == String("dumpReg")) //dump all register values
-      radio.readAllRegs();
-
-    if (incomingCMD == String("enEncrypt")) //enable encryption
-      radio.encrypt(KEY);
-  
-    if (incomingCMD == String("disEncrypt")) //disable encryption
-      radio.encrypt(null);
-     
-    if (incomingCMD == String("dumpFlash")) //dump flash area
-      {
-        Serial.println("Flash content:");
-        int counter = 0;
-  
-         while(counter<=256){
-          Serial.print(flash.readByte(counter++), HEX);
-          Serial.print('.');
-        }
-        while(flash.busy());
-        Serial.println();
+    if (incomingCMD == String("FormatChip")) {    //erases chip
+      Serial.print("Formatting flash chip ... ");
+      flash.chipErase();
+      while(flash.busy());
+      Serial.println("Flash formatted for datalogging.");
       }
 
-    if (incomingCMD == String("eraseChip")) //erase flash chip
-      {
-        Serial.print("Deleting Flash chip content... ");
-        flash.chipErase();
-        while(flash.busy());
-        Serial.println("DONE");
-       }
-
-    if (incomingCMD == String("ID"))//flash ID
-      {
-        Serial.print("DeviceID: ");
-        word jedecid = flash.readDeviceId();
-        Serial.println(jedecid, HEX);
-      }
+    if (incomingCMD == String("WriteData")) { //0-9
+      average = random(0,254);  //for testing purposes
+      getOffset();
+      Serial.print(average); Serial.print(" written to offset ");Serial.println(DataOffset);
+      flash.writeByte(DataOffset,average);
+      PrevDataOffset=DataOffset;
+      DataOffset++;
     }
 
-
-    if (incomingCMD == String("XMIT")) //transmit
-      {
-      //fill in the struct with new values
-      theData.cmd = 'W';
-      //theData.uptime = millis();
-      theData.volume = average;
-      
-      Serial.print("Sending struct (");
-      Serial.print(sizeof(theData));
-      Serial.print(" bytes) ... ");
-      if (radio.sendWithRetry(GATEWAYID, (const void*)(&theData), sizeof(theData)))
-        Serial.print(" ok!");
-      else Serial.print(" nothing...");
-      Serial.println();
-      Blink(LED,3);
+    if (incomingCMD == String("ReadChip")) { //d=dump flash area
+      Serial.println("Reading flash memory content:");
+      getOffset();
+      long timestamp = DataInterval;
+      long counter = 0;
+      while(counter<DataOffset){
+        Serial.print("Time Stamp (min):\t");
+        Serial.print(timestamp);Serial.print("\t");
+        Serial.print("Volume (gal):\t"); Serial.println(flash.readByte(counter++), DEC);
+        timestamp = timestamp + DataInterval;
       }
-
+      Serial.print("Data read from flash memory until offset ");
+      Serial.print(DataOffset);
+      Serial.println(".");
+    }
+      
+  }
 }
+ 
